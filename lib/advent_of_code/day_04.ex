c2342c18ef78a003e9ec204f1e5999514febb1df
Eleni Lixourioti <contact@eleni.co>
2022-12-04 09:22:09 +0100

    Add Day 04 solutions

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,50 @@
 defmodule AdventOfCode.Day04 do
-  def part1(_args) do
+  @moduledoc """
+  See: https://adventofcode.com/2022/day/3
+  """
+
+  def part1(input) do
+    solve(input, &either_contain?/2)
   end
 
-  def part2(_args) do
+  def part2(input) do
+    solve(input, &overlap?/2)
+  end
+
+  defp solve(input, compare) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(0, fn line, result ->
+      [range_one, range_two] = line_to_ranges(line)
+
+      if compare.(range_one, range_two) do
+        result + 1
+      else
+        result
+      end
+    end)
+  end
+
+  defp line_to_ranges(line) do
+    line
+    |> String.split(",")
+    |> Enum.map(&parse_range/1)
+  end
+
+  defp parse_range(raw_range) do
+    raw_range
+    |> String.split("-")
+    |> Enum.map(&String.to_integer/1)
+    |> then(fn [start, finish] -> start..finish end)
+  end
+
+  defp either_contain?(range_one, range_two) do
+    (range_one.first >= range_two.first and range_one.last <= range_two.last) or
+      (range_two.first >= range_one.first and range_two.last <= range_one.last)
+  end
+
+  defp overlap?(range_one, range_two) do
+    range_one.first in range_two or range_one.last in range_two or
+      range_two.first in range_one or range_two.last in range_one
   end
 end
