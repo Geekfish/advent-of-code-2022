6afd0066efa36696cafb2dd12e43193d96f29d72
Eleni Lixourioti <contact@eleni.co>
2022-12-20 10:04:25 +0100

    Add Day 08 solutions

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -1,7 +1,98 @@
 defmodule AdventOfCode.Day08 do
-  def part1(_args) do
+  def part1(input) do
+    input
+    |> parse_forest()
+    |> count_visible()
   end
 
-  def part2(_args) do
+  def part2(input) do
+    input
+    |> parse_forest()
+    |> get_max_scenic_score()
+  end
+
+  defp parse_forest(raw_input) do
+    lines = String.split(raw_input, "\n", trim: true)
+
+    trees =
+      lines
+      |> Enum.with_index()
+      |> Enum.flat_map(fn {raw_row, row_index} ->
+        raw_row
+        |> String.codepoints()
+        |> Enum.map(&String.to_integer/1)
+        |> Enum.with_index()
+        |> Enum.map(fn {tree_height, col_index} ->
+          {{row_index, col_index}, tree_height}
+        end)
+      end)
+      |> Map.new()
+
+    %{
+      max_index: length(lines) - 1,
+      trees: trees
+    }
+  end
+
+  defp count_visible(forest) do
+    forest.trees
+    |> Enum.reduce(0, fn {position, height}, num_visible ->
+      if tree_visible?(forest.trees, forest.max_index, position, height) do
+        num_visible + 1
+      else
+        num_visible
+      end
+    end)
+  end
+
+  defp tree_visible?(trees, max_index, {row_index, col_index}, tree_height) do
+    on_boundary?(max_index, {row_index, col_index}) or
+      visible_in_line(tree_height, &trees[{&1, col_index}], 0..(row_index - 1)) or
+      visible_in_line(tree_height, &trees[{&1, col_index}], (row_index + 1)..max_index) or
+      visible_in_line(tree_height, &trees[{row_index, &1}], 0..(col_index - 1)) or
+      visible_in_line(tree_height, &trees[{row_index, &1}], (col_index + 1)..max_index)
+  end
+
+  defp visible_in_line(current_height, get_height_fn, range) do
+    range
+    |> Enum.map(get_height_fn)
+    |> Enum.all?(fn other_height -> other_height < current_height end)
+  end
+
+  defp on_boundary?(_max_index, {0, _col_index}), do: true
+  defp on_boundary?(_max_index, {_row_index, 0}), do: true
+  defp on_boundary?(max_index, {max_index, _col_index}), do: true
+  defp on_boundary?(max_index, {_row_index, max_index}), do: true
+  defp on_boundary?(_, _), do: false
+
+  defp get_max_scenic_score(forest) do
+    forest.trees
+    |> Enum.reduce(0, fn {position, height}, max_scenic_score ->
+      case get_scenic_score(forest.trees, forest.max_index, position, height) do
+        new_scenic_score when new_scenic_score > max_scenic_score -> new_scenic_score
+        _other -> max_scenic_score
+      end
+    end)
+  end
+
+  defp get_scenic_score(trees, max_index, {row_index, col_index}, tree_height) do
+    if on_boundary?(max_index, {row_index, col_index}) do
+      0
+    else
+      count_visible_in_view(tree_height, &trees[{&1, col_index}], (row_index - 1)..0) *
+        count_visible_in_view(tree_height, &trees[{&1, col_index}], (row_index + 1)..max_index) *
+        count_visible_in_view(tree_height, &trees[{row_index, &1}], (col_index - 1)..0) *
+        count_visible_in_view(tree_height, &trees[{row_index, &1}], (col_index + 1)..max_index)
+    end
+  end
+
+  defp count_visible_in_view(current_height, get_height_fn, range) do
+    Enum.reduce_while(range, 0, fn tree_index, count ->
+      if get_height_fn.(tree_index) >= current_height do
+        {:halt, count + 1}
+      else
+        {:cont, count + 1}
+      end
+    end)
   end
 end
--- test/advent_of_code/day_08_test.exs
+++ test/advent_of_code/day_08_test.exs
@@ -3,19 +3,45 @@ defmodule AdventOfCode.Day08Test do
 
   import AdventOfCode.Day08
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @input """
+  30373
+  25512
+  65332
+  33549
+  35390
+  """
 
-    assert result
+  test "part1" do
+    result = part1(@input)
+
+    # XXXXX
+    # XXX1X
+    # XX3XX
+    # X3X4X
+    # XXXXX
+    # All the trees on the periphery (16)
+    # + the ones that are visible from any direction (5).
+
+    assert 21 == result
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    result = part2(@input)
 
-    assert result
+    # 30373
+    # 25512
+    # 65332
+    # 33X49
+    # 35390
+    #
+    # (Where X is 5)
+    # Looking up, its view is blocked at 2 trees (by another tree with a height of 5).
+    # Looking left, its view is not blocked; it can see 2 trees.
+    # Looking down, its view is also not blocked; it can see 1 tree.
+    # Looking right, its view is blocked at 2 trees (by a massive tree of height 9).
+
+    # This tree's scenic score is 8 (2 * 2 * 1 * 2)
+
+    assert 8 == result
   end
 end
